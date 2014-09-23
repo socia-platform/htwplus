1af4e7b054d89a90c9983d4e3d5d92ae34166661
Ryad El-Dajani <ryad.eldajani@gmail.com>
2014-09-23 15:41:10 +0200

    This commit contains a refactoring of the LDAP connection handling to enable access for all users of the HTW Berlin.
    Classes:
    - AccountController: Code and naming improvements, add of email for user, add i18n, add JavaDoc
    - LDAPConnector: Complete refactor with configurable values, code improvements, add JavaDoc
    
    Templates:
    - All templates are refactored due to the naming refactoring of Notification
    
    Other:
    - application.sample.conf: Add default parameters for LDAP connector
    - messages: Add i18n for LDAP connector and authenticate controller

--- app/controllers/AccountController.java
+++ app/controllers/AccountController.java
@@ -6,24 +6,31 @@ import java.util.Random;
 
 import models.Account;
 import models.LDAPConnector;
-import models.LDAPConnector.LDAPConnectorException;
 import models.enums.AccountRole;
 import models.Login;
 import play.Logger;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.db.jpa.Transactional;
+import play.i18n.Messages;
 import play.mvc.Result;
 import views.html.index;
 
+/**
+ * Controller for authenticate purposes.
+ */
 @Transactional
 public class AccountController extends BaseController {
-
 	/**
 	 * Defines a form wrapping the Account class.
 	 */
 	final static Form<Account> signupForm = form(Account.class);
 
+    /**
+     * Default authentication action.
+     *
+     * @return Result
+     */
 	public static Result authenticate() {
 		DynamicForm form = form().bindFromRequest();
 		String username = form.field("email").value();
@@ -31,63 +38,73 @@ public class AccountController extends BaseController {
 		if (username.contains("@")) {
 			return defaultAuthenticate();
 		} else if (username.length() == 0) {
-			flash("error", "Also deine Matrikelnummer brauchen wir schon!");
+			flash("error", Messages.get("authenticate.matriculationNumberMissing"));
 			return badRequest(index.render());
 		} else {
 			return LDAPAuthenticate();
 		}
 	}
 
+    /**
+     * LDAP authentication.
+     *
+     * @return Result
+     */
 	private static Result LDAPAuthenticate() {
 		Form<Login> form = form(Login.class).bindFromRequest();
-		String username = form.field("email").value();
+		String matriculationNumber = form.field("email").value();
 		String password = form.field("password").value();
 		String rememberMe = form.field("rememberMe").value();
 		
 		// Clean the username
-		username = username.trim().toLowerCase();
+		matriculationNumber = matriculationNumber.trim().toLowerCase();
 
+        // establish LDAP connection and try to get user data
 		LDAPConnector ldap = new LDAPConnector();
 		try {
-			ldap.connect(username, password);
-		} catch (LDAPConnectorException e) {
+			ldap.connect(matriculationNumber, password);
+		} catch (LDAPConnector.LdapConnectorException e) {
 			flash("error", e.getMessage());
 			Component.addToContext(Component.ContextIdent.loginForm, form);
 			return badRequest(index.render());
 		}
 
-		Account account = Account.findByLoginName(ldap.getUsername());
+        // try to find user in DB, set role if found (default STUDENT role)
+		Account account = Account.findByLoginName(matriculationNumber);
 		AccountRole role = AccountRole.STUDENT;
-		if(ldap.getRole() != null) {
+		if (ldap.getRole() != null) {
 			role = ldap.getRole();
 		}
-		
+
+        // if user is not found in DB, create new user from LDAP data, otherwise update user data
 		if (account == null) {
 			account = new Account();
-			Logger.info("New Account for " + ldap.getUsername()
-					+ " will be created.");
-			account.firstname = ldap.getFirstname();
-			account.lastname = ldap.getLastname();
-			account.loginname = ldap.getUsername();
+			Logger.info("New Account for " + matriculationNumber + " will be created.");
+			account.firstname = ldap.getFirstName();
+			account.lastname = ldap.getLastName();
+			account.loginname = matriculationNumber;
+            account.email = ldap.getEmail();
 			account.password = "LDAP - not needed";
 			Random generator = new Random();
 			account.avatar = "a" + generator.nextInt(10);
 			account.role = role;
 			account.create();
 		} else {
-			account.firstname = ldap.getFirstname();
-			account.lastname = ldap.getLastname();
+			account.firstname = ldap.getFirstName();
+			account.lastname = ldap.getLastName();
+            account.email = ldap.getEmail();
 			account.role = role;
 			account.update();
 		}
 
+        // re-create session, set user
 		session().clear();
 		session("id", account.id.toString());
-		if(rememberMe != null){
+		if (rememberMe != null) {
 			session("rememberMe", "1");
 		}
 
-		return redirect(routes.Application.index());
+		return redirect(controllers.routes.Application.index());
 	}
 
 	private static Result defaultAuthenticate() {
@@ -99,15 +116,13 @@ public class AccountController extends BaseController {
 		} else {
 			session().clear();
 			session("email", loginForm.get().email);
-			session("id",
-					Account.findByEmail(loginForm.get().email).id.toString());
-			session("firstname",
-					Account.findByEmail(loginForm.get().email).firstname);
-			if(loginForm.get().rememberMe != null){
+			session("id", Account.findByEmail(loginForm.get().email).id.toString());
+			session("firstname", Account.findByEmail(loginForm.get().email).firstname);
+			if (loginForm.get().rememberMe != null) {
 				session("rememberMe", "1");
 			}
 			
-			return redirect(routes.Application.index());
+			return redirect(controllers.routes.Application.index());
 		}
 	}
 
@@ -116,8 +131,8 @@ public class AccountController extends BaseController {
 	 */
 	public static Result logout() {
 		session().clear();
-		flash("success", "Du bist nun ausgeloggt");
-		return redirect(routes.Application.index());
-	}
+		flash("success", Messages.get("authenticate.logout"));
 
+		return redirect(controllers.routes.Application.index());
+	}
 }
--- app/models/LDAPConnector.java
+++ app/models/LDAPConnector.java
@@ -8,101 +8,177 @@ import org.apache.directory.api.ldap.model.entry.Entry;
 import org.apache.directory.api.ldap.model.exception.LdapException;
 import org.apache.directory.api.ldap.model.message.SearchScope;
 import org.apache.directory.ldap.client.api.LdapConnection;
+import org.apache.directory.ldap.client.api.LdapConnectionConfig;
 import org.apache.directory.ldap.client.api.LdapNetworkConnection;
 import org.apache.directory.ldap.client.api.exception.InvalidConnectionException;
 
 import play.Logger;
+import play.Play;
+import play.i18n.Messages;
 
+/**
+ * LDAP Connector to establish LDAP connection and request user account data from directory.
+ */
 public class LDAPConnector {
-	
-	private String server = "portia.f4.htw-berlin.de";
-	private int port = 389;
-	private String root = "ou=users,o=f4,dc=htw-berlin,dc=de";
-	private String groupRoot = "ou=groups,o=f4,dc=htw-berlin,dc=de";
-	private String studentIdent = "student";
-	private String tutorIdent = "lehrende";
-	
-	private String username = null;
-	private String password = null;
-	private String firstname = null;
-	private String lastname = null;
+    /**
+     * First name
+     */
+    private String firstName = null;
 
-	private AccountRole role = null;
-	
-	LdapConnection conn = null;
-		
-	public String getUsername() {
-		return username;
-	}
+    /**
+     * Last name
+     */
+    private String lastName = null;
 
-	public String getFirstname() {
-		return firstname;
-	}
+    /**
+     * E-Mail
+     */
+    private String email = null;
 
-	public String getLastname() {
-		return lastname;
-	}
-	
-	public AccountRole getRole() {
-		return role;
-	}
-	
-	public LDAPConnector() {
-		conn = new LdapNetworkConnection(server, port);
-	}
-	
-	public void connect(String username, String password) throws LDAPConnectorException {
-		this.username = username;
-		this.password = password;
-		
-		try {
-			conn.bind("uid=" + this.username + "," + this.root, this.password);
-		} catch (InvalidConnectionException e) {
-			e.printStackTrace();
-			throw new LDAPConnectorException("Keine Verbindung zum LDAP-Server möglich. Bitte versuche es später noch einmal.");
-		} catch (LdapException e) {
-			e.printStackTrace();
-			throw new LDAPConnectorException("Ungültiges Passwort oder Nutzername.");
-		}
-	
-		EntryCursor entCursor = null;
-    	try {
-			entCursor =  conn.search(this.root, "(uid=" + this.username + ")", SearchScope.ONELEVEL, "*");
-			entCursor.next();
-			Entry entry = entCursor.get();
-			this.firstname = entry.get("givenName").getString();
-			this.lastname = entry.get("sn").getString();
-			Logger.info("Read Account from LDAP: " + this.firstname + " " + this.lastname);
-		} catch (LdapException | CursorException e) {
-			e.printStackTrace();
-			throw new LDAPConnectorException("Es gab ein Problem bei der Verbindung zum LDAP-Server. Bitte versuche es später noch einmal.");
-		}
-    	
-    	try {
-			entCursor =  conn.search(this.groupRoot, "(memberUid=" + this.username + ")", SearchScope.ONELEVEL, "*");
-			String role = null;
-	    	while(entCursor.next()) {
-	    		Entry entry = entCursor.get();
-	    		role = entry.get("cn").getString();
-	    		if(role.equals(this.studentIdent)) {
-	    			this.role = AccountRole.STUDENT;
-	    		}
-	    		if(role.equals(this.tutorIdent)){
-	    			this.role = AccountRole.TUTOR;
-	    		}
-	    	}
-		} catch (LdapException | CursorException e) {
-			e.printStackTrace();
-			throw new LDAPConnectorException("Es gab ein Problem bei der Verbindung zum LDAP-Server. Bitte versuche es später noch einmal.");
-		}
-    	
-	}
-		
-	public class LDAPConnectorException extends Exception{
-		public LDAPConnectorException(String message) {
-			super(message);
-		}
+    /**
+     * Account role
+     */
+    private AccountRole role = null;
 
-		private static final long serialVersionUID = 1L;
-	}
+    /**
+     * LDAP connection reference
+     */
+    LdapConnection connection = null;
+
+    /**
+     * Getter for first name.
+     *
+     * @return First name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Getter for last name.
+     *
+     * @return Last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Getter for email.
+     *
+     * @return E-Mail
+     */
+    public String getEmail() {
+        return this.email;
+    }
+
+    /**
+     * Getter for account role.
+     *
+     * @return Account role
+     */
+    public AccountRole getRole() {
+        return role;
+    }
+
+    /**
+     * Constructor.
+     */
+    public LDAPConnector() {
+        String server = Play.application().configuration().getString("ldap.server");
+        int port = Integer.parseInt(Play.application().configuration().getString("ldap.port"));
+        boolean startTls = Boolean.parseBoolean(Play.application().configuration().getString("ldap.startTls"));
+
+        LdapConnectionConfig connectionConfig = new LdapConnectionConfig();
+        connectionConfig.setLdapHost(server);
+        connectionConfig.setLdapPort(port);
+        connectionConfig.setUseTls(startTls);
+
+        this.connection = new LdapNetworkConnection(connectionConfig);
+    }
+
+    /**
+     * Connects to the LDAP server. If successful connected, the user is searched. If found, the user
+     * data is read and set into firstName and lastName.
+     *
+     * @param userName User
+     * @param password Password
+     * @throws LdapConnectorException
+     */
+    public void connect(String userName, String password) throws LdapConnectorException {
+        // connection and search related parameters
+        String userRoot = Play.application().configuration().getString("ldap.userRoot");
+        String groupRoot = Play.application().configuration().getString("ldap.groupRoot");
+        String connectionBind = Play.application().configuration().getString("ldap.connectionBind")
+                .replace("%USER%", userName)
+                .replace("%USER_ROOT%", userRoot);
+        String userSearch = Play.application().configuration().getString("ldap.userSearch")
+                .replace("%USER%", userName);
+        String groupSearch = Play.application().configuration().getString("ldap.groupSearch")
+                .replace("%BIND%", connectionBind);
+
+        // LDAP keys for values from LDAP server
+        String userFirstName = Play.application().configuration().getString("ldap.serverValues.firstName");
+        String userLastName = Play.application().configuration().getString("ldap.serverValues.lastName");
+        String userEmail = Play.application().configuration().getString("ldap.serverValues.email");
+        String groupName = Play.application().configuration().getString("ldap.serverValues.groupName");
+        String studentRole = Play.application().configuration().getString("ldap.serverValues.studentRole");
+        String tutorRole = Play.application().configuration().getString("ldap.serverValues.tutorRole");
+
+        // try to connect to the LDAP server
+        try {
+            this.connection.bind(connectionBind, password);
+        } catch (InvalidConnectionException e) {
+            e.printStackTrace();
+            throw new LdapConnectorException(Messages.get("ldap.noConnection"));
+        } catch (LdapException e) {
+            e.printStackTrace();
+            throw new LdapConnectorException(Messages.get("ldap.wrongCredentials"));
+        }
+
+        // login to LDAP successful, try to find the user data
+        EntryCursor entCursor;
+        try {
+            entCursor = this.connection.search(userRoot, userSearch, SearchScope.ONELEVEL, "*");
+            entCursor.next();
+            Entry entry = entCursor.get();
+            this.firstName = entry.get(userFirstName).getString();
+            this.lastName = entry.get(userLastName).getString();
+            this.email = entry.get(userEmail).getString();
+            Logger.info("Read Account from LDAP: " + this.firstName + " " + this.lastName + " (E-Mail: " + this.email + ")");
+        } catch (LdapException | CursorException e) {
+            e.printStackTrace();
+            throw new LdapConnectorException(Messages.get("ldap.generalError"));
+        }
+
+        // user data successfully set, try to find the role of the user
+        try {
+            entCursor =  connection.search(groupRoot, groupSearch, SearchScope.ONELEVEL, "*");
+            String role;
+            while (entCursor.next()) {
+                Entry entry = entCursor.get();
+                role = entry.get(groupName).getString();
+                if (role.equals(studentRole)) {
+                    this.role = AccountRole.STUDENT;
+                }
+                if (role.equals(tutorRole)) {
+                    this.role = AccountRole.TUTOR;
+                }
+            }
+        } catch (LdapException | CursorException e) {
+            e.printStackTrace();
+            throw new LdapConnectorException(Messages.get("ldap.generalError"));
+        }
+    }
+
+    /**
+     * Exception class for exceptions thrown in LdapConnector.
+     */
+    public class LdapConnectorException extends Exception{
+        public LdapConnectorException(String message) {
+            super(message);
+        }
+
+        private static final long serialVersionUID = 1L;
+    }
 }
