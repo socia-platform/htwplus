05964d94a54bde52f04840e33fb0c253709e558a
Kevin van Bernum <kevin.vanbernum@gmail.com>
2014-09-28 19:39:18 +0200

    #17 next steps

--- app/assets/javascripts/landingpage.js
+++ app/assets/javascripts/landingpage.js
@@ -1,7 +1,26 @@
-$(window).resize(function() {
-	if ((800 / 1200 * $('div#login').innerWidth()) < $('div#login').innerHeight()) {
-		$('div#login').css('background-size', 'auto 100%');
+var imgHeight;
+var imgWidth;
+
+function resizeBackground() {
+	if ((imgHeight / imgWidth * $('div#hp-login').innerWidth()) < $('div#hp-login').innerHeight()) {
+		$('div#hp-login').css('background-size', 'auto 100%');
 	} else {
-		$('div#login').css('background-size', '100% auto');
+		$('div#hp-login').css('background-size', '100% auto');
 	}
+}
+
+$(window).load(function() {
+    var newImg = new Image();
+    newImg.src = 'http://www.htw-berlin.de/fileadmin/HTW/Zentral/DE/HTW/ZR1_Presse/Pressefotos/130719___Philipp_Meuser_0011_01_1200px_crop.jpg';
+    imgHeight = newImg.height;
+    imgWidth = newImg.width;
+    resizeBackground();
 });
+
+$(window).resize(resizeBackground);
+
+$('#hp-navbar').affix({
+    offset: {
+        top: function () { return $('#hp-login').outerHeight(); }
+    }
+})
