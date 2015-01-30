53652a7080501559d3b7f696ada935c51c655a5b
IvenZ <iven@styla.com>
2015-01-30 14:05:22 +0100

    #69 refactor query string back to filtered query

--- app/models/services/ElasticsearchService.java
+++ app/models/services/ElasticsearchService.java
@@ -5,6 +5,7 @@ import controllers.Component;
 import models.*;
 import models.enums.GroupType;
 import models.enums.LinkType;
+import org.apache.lucene.queries.TermFilter;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexResponse;
@@ -35,6 +36,10 @@ public class ElasticsearchService {
     private static final String ES_GROUP_MAPPING = ConfigFactory.load().getString("elasticsearch.groupMapping");
     private static final String ES_POST_MAPPING = ConfigFactory.load().getString("elasticsearch.postMapping");
     private static final String ES_INDEX = ConfigFactory.load().getString("elasticsearch.index");
+    private static final String ES_TYPE_USER = ConfigFactory.load().getString("elasticsearch.userType");
+    private static final String ES_TYPE_GROUP = ConfigFactory.load().getString("elasticsearch.groupType");
+    private static final String ES_TYPE_POST = ConfigFactory.load().getString("elasticsearch.postType");
+    private static final int ES_RESULT_SIZE = ConfigFactory.load().getInt("elasticsearch.search.limit");
 
     private ElasticsearchService() {
         client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
@@ -66,21 +71,21 @@ public class ElasticsearchService {
     }
 
     public static void createMapping() throws IOException {
-        getInstance().getClient().admin().indices().preparePutMapping(ES_INDEX).setType("user")
+        getInstance().getClient().admin().indices().preparePutMapping(ES_INDEX).setType(ES_TYPE_USER)
                 .setSource(ES_USER_MAPPING)
                 .execute().actionGet();
 
-        getInstance().getClient().admin().indices().preparePutMapping(ES_INDEX).setType("post")
+        getInstance().getClient().admin().indices().preparePutMapping(ES_INDEX).setType(ES_TYPE_POST)
                 .setSource(ES_POST_MAPPING)
                 .execute().actionGet();
 
-        getInstance().getClient().admin().indices().preparePutMapping(ES_INDEX).setType("group")
+        getInstance().getClient().admin().indices().preparePutMapping(ES_INDEX).setType(ES_TYPE_GROUP)
                 .setSource(ES_GROUP_MAPPING)
                 .execute().actionGet();
     }
 
     public static void indexPost(Post post) throws IOException {
-        IndexResponse indexResponse = getInstance().getClient().prepareIndex(ES_INDEX, "post", post.id.toString())
+        IndexResponse indexResponse = getInstance().getClient().prepareIndex(ES_INDEX, ES_TYPE_POST, post.id.toString())
                 .setSource(jsonBuilder()
                         .startObject()
                         .field("content", post.content)
@@ -95,11 +100,12 @@ public class ElasticsearchService {
     }
 
     public static void indexGroup(Group group) throws IOException {
-        IndexResponse indexResponse = getInstance().getClient().prepareIndex(ES_INDEX, "group", group.id.toString())
+        IndexResponse indexResponse = getInstance().getClient().prepareIndex(ES_INDEX, ES_TYPE_GROUP, group.id.toString())
                 .setSource(jsonBuilder()
                         .startObject()
                         .field("title", group.title)
                         .field("grouptype", group.groupType)
+                        .field("public", true)
                         .field("owner", group.owner.id)
                         .field("member", GroupAccount.findAccountIdsByGroup(group, LinkType.establish))
                         .endObject())
@@ -109,11 +115,12 @@ public class ElasticsearchService {
     }
 
     public static void indexAccount(Account account) throws IOException {
-        IndexResponse response = getInstance().getClient().prepareIndex(ES_INDEX, "user", account.id.toString())
+        IndexResponse response = getInstance().getClient().prepareIndex(ES_INDEX, ES_TYPE_USER, account.id.toString())
                 .setSource(jsonBuilder()
                                 .startObject()
                                 .field("name", account.name)
                                 .field("avatar", account.avatar)
+                                .field("public", true)
                                 .field("friends", Friendship.findFriendsId(account))
                                 .endObject()
                 )
@@ -126,6 +133,8 @@ public class ElasticsearchService {
     /**
      * Build search query based on all provided fields
      * @param query - Terms to search for (e.g. 'informatik')
+     * @param filter - Filter for searchfacets (e.g. user, group, comment)
+     * @param page - Which results should be shown (e.g. 1: 1-10 ; 2: 11-20 etc.)
      * @param currentAccountId - AccountId from user who is logged in (for scoring)
      * @param mustFields - All fields to search on
      * @param scoringFields - All fields which affect the scoring
@@ -135,19 +144,23 @@ public class ElasticsearchService {
      */
     public static SearchResponse doSearch(String query, String filter, int page, String currentAccountId, List<String> mustFields, List<String> scoringFields) throws ExecutionException, InterruptedException {
 
-         // Build searchQuery by provided fields (mustFields) to search on
-        QueryBuilder searchQuery = QueryBuilders.queryString(query);
+        // Build searchQuery by provided fields (mustFields) to search on
+        QueryBuilder searchQuery = QueryBuilders.multiMatchQuery(query, mustFields.toArray(new String[mustFields.size()]));
 
         // Build scoringQuery by provided fields (shouldFields) to increase the scoring of a better matching hit
-        QueryBuilder scoringQuery = QueryBuilders.multiMatchQuery(currentAccountId, scoringFields.toArray(new String[scoringFields.size()])).operator(MatchQueryBuilder.Operator.OR);
+        QueryBuilder scoringQuery = QueryBuilders.multiMatchQuery(currentAccountId, scoringFields.toArray(new String[scoringFields.size()]));
 
         // Build completeQuery with search- and scoringQuery
         QueryBuilder completeQuery = QueryBuilders.boolQuery().must(searchQuery).should(scoringQuery);
 
+        FilterBuilder viewableFilter = FilterBuilders.boolFilter().should(FilterBuilders.termFilter("viewable", currentAccountId),FilterBuilders.termFilter("public", true));
+
+        QueryBuilder filteredQuery = QueryBuilders.filteredQuery(completeQuery, viewableFilter);
+
         // Build searchRequest which will be executed after fields to highlight are added.
         SearchRequestBuilder searchRequest = ElasticsearchService.getInstance().getClient().prepareSearch(ES_INDEX)
                 .setFetchSource(new String[]{"title", "grouptype", "name", "avatar", "content"}, new String[]{})
-                .setQuery(completeQuery);
+                .setQuery(filteredQuery);
 
         // Add highlighting on all fields to search on
         for (String field : mustFields) {
@@ -158,7 +171,7 @@ public class ElasticsearchService {
         searchRequest = searchRequest.setHighlighterPreTags("<strong>").setHighlighterPostTags("</strong>");
 
         // Enable pagination
-        searchRequest = searchRequest.setFrom((page * 10) - 10);
+        searchRequest = searchRequest.setFrom((page * ES_RESULT_SIZE) - ES_RESULT_SIZE);
 
         // Add term aggregation for facet count
         searchRequest = searchRequest.addAggregation(AggregationBuilders.terms("types").field("_type"));
@@ -177,13 +190,19 @@ public class ElasticsearchService {
     }
 
     public static void deleteGroup(Group group) {
-        DeleteResponse response = client.prepareDelete(ES_INDEX, "group", group.id.toString())
+        DeleteResponse response = client.prepareDelete(ES_INDEX, ES_TYPE_GROUP, group.id.toString())
                 .execute()
                 .actionGet();
     }
 
     public static void deletePost(Post post) {
-        DeleteResponse response = client.prepareDelete(ES_INDEX, "post", post.id.toString())
+        DeleteResponse response = client.prepareDelete(ES_INDEX, ES_TYPE_POST, post.id.toString())
+                .execute()
+                .actionGet();
+    }
+
+    public static void deleteAccount(Account account) {
+        DeleteResponse response = client.prepareDelete(ES_INDEX, ES_TYPE_USER, account.id.toString())
                 .execute()
                 .actionGet();
     }
