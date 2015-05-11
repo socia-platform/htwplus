9c072781fce714733237104fe126713de002f955
TeNNoX <manuel.tennox@gmail.com>
2015-05-12 01:36:16 +0200

    #43 Make single notification mail subject more specific
    [RFC] I think it makes for a better user experience.
    People hate it to get E-Mails with generic subjects, and tend to just delete them.

--- app/models/services/EmailService.java
+++ app/models/services/EmailService.java
@@ -79,7 +79,8 @@ public class EmailService {
         try {
             String subject = notifications.size() > 1
                     ? Messages.get("notification.email_notifications.collected.subject", notifications.size())
-                    : Messages.get("notification.email_notifications.single.subject");
+                    : Messages.get("notification.email_notifications.single.subject_specific",
+                        notifications.get(0).rendered.replaceAll("<[^>]*>", ""));
             // send the email
             this.sendEmail(subject, recipient.name + " <" + recipient.email + ">",
                     TemplateService.getInstance().getRenderedTemplate(EmailService.PLAIN_TEXT_TEMPLATE, notifications, recipient),
