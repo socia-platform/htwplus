1996b21047b9c9e951f664cf96d56f7e76a0e442
IvenZ <iven@styla.com>
2015-06-10 18:19:28 +0200

    #119 clean header navigation and breadcrumb

--- app/assets/javascripts/htwplus.js
+++ app/assets/javascripts/htwplus.js
@@ -383,7 +383,7 @@ $(document).ready(function () {
 
     autoSuggestResult.initialize();
 
-    $('.hp-easy-search').typeahead(
+    $('.hp-search-form .form-control').typeahead(
         {
             hint: true,
             highlight: false,
@@ -432,7 +432,7 @@ $('body').popover({
 });
 
 $('.hp-focus-search').click(function() {
-    $('.hp-easy-search').focus();
+    $('#hp-search').focus();
 });
 
 /*
