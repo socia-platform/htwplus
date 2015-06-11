98d5fc9579df9f51eb9f8baa38e2bbea889d833f
Roechi <msg.remus@gmail.com>
2015-06-11 14:02:21 +0200

    Created Expose annotation to declare itemizable fields of Model.

---
+++ app/util/Expose.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by richard on 11.06.15.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Expose {
+
+}
