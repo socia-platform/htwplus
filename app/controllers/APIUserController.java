f9507b576ca4fa39cdfcdc590f6fd6b1a0f5a98f
Tammo Behrends <behr.tam@gmail.com>
2015-06-24 15:51:37 +0200

    Extended get() action method to also serve all users.

--- app/controllers/APIUserController.java
+++ app/controllers/APIUserController.java
@@ -3,53 +3,55 @@ package controllers;
 import models.Account;
 import models.enums.CustomContentType;
 import net.hamnaberg.json.*;
-import play.mvc.Http;
+import net.hamnaberg.json.Error;
 import play.mvc.Result;
-import play.mvc.Results;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-/**
- * Created by richard on 17.06.15.
- */
-public class APIUserController extends BaseController{
 
-    public static Result view(final Long id) {
-        Account account = Account.findById(id);
+public class APIUserController extends BaseController {
 
+    public static Result get(final Long id) {
         if (request().getHeader("Accept").contains(CustomContentType.JSON_COLLECTION.getIdentifier())) {
-            Collection collection;
-            if (account == null) {
-                collection = Collection.create(
-                        URI.create(request().host() + request().path()),
-                        new ArrayList<Link>(),
-                        new ArrayList<Item>(),
-                        new ArrayList<Query>(),
-                        account.getTemplate(),
-                        net.hamnaberg.json.Error.create("Account not found", "404", "The " +
-                                "requested account does not seem to exist.")
-                );
-            } else {
-                URI uri = URI.create(request().host() + request().path());
-                ArrayList<Item> items = new ArrayList<Item>();
-                items.add(Item.create(uri, account.getProperies()));
-                collection = Collection.create(
-                        uri,
-                        new ArrayList<Link>(),
-                        items,
-                        new ArrayList<Query>(),
-                        account.getTemplate(),
-                        net.hamnaberg.json.Error.create("none", "none", "none")
-                );
-            }
-            response().setContentType(CustomContentType.JSON_COLLECTION.getIdentifier());
+            URI uri = URI.create(request().host() + request().path());
+            Error error = Error.EMPTY;
+            List<Item> items = null;
 
+            if (id == 0) {  // http GET .../users
+                items = Account.all()
+                        .stream()
+                        .map(a -> Item.create(URI.create(uri.toString() + "/" + a.id), a.getProperies()))
+                        .collect(Collectors.toList());
+
+            } else {  // http GET .../users/:id
+                Account account = Account.findById(id);
+                items = new ArrayList<Item>();
+                if (account == null) {
+                    error = net.hamnaberg.json.Error.create("Account not found", "404", "The " +
+                                                            "requested account does not seem to exist.");
+                } else {
+                    items.add(Item.create(uri, account.getProperies()));
+                }
+            }
+
+            Collection collection = Collection.create(
+                    uri,
+                    new ArrayList<Link>(),
+                    items,
+                    new ArrayList<Query>(),
+                    Template.create(Account.EXAMPLE.getProperies()),
+                    error
+            );
+
+            response().setContentType(CustomContentType.JSON_COLLECTION.getIdentifier());
             return ok(collection.toString());
         } else {
-            response().setHeader("Warning", "Only accepting Accept header: " + CustomContentType.JSON_COLLECTION.getIdentifier());
+            response().setHeader("Warning", "Only accepting Accept header: " +
+                                 CustomContentType.JSON_COLLECTION.getIdentifier());
             return status(NOT_ACCEPTABLE);
         }
     }
-    
 }
