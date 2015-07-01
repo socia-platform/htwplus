c280021596cb07028fcb12823e71682cf6d95b71
Roechi <msg.remus@gmail.com>
2015-07-01 22:28:00 +0200

    Added finder methods.

--- app/models/Client.java
+++ app/models/Client.java
@@ -4,6 +4,7 @@ import models.base.BaseModel;
 import play.db.jpa.JPA;
 
 import javax.persistence.Entity;
+import javax.persistence.NoResultException;
 import java.net.URI;
 
 /**
@@ -28,5 +29,19 @@ public class Client extends BaseModel {
     @Override
     public void delete() {
 
+    }
+
+    public static Client findById(Long id) {
+        return JPA.em().find(Client.class, id);
+    }
+
+    public static Client findByClientId(String clientId) {
+        try{
+            return (Client) JPA.em()
+                    .createQuery("from Client a where a.clientId = :clientId")
+                    .setParameter("clientId", clientId).getSingleResult();
+        } catch (NoResultException exp) {
+            return null;
+        }
     }
 }
