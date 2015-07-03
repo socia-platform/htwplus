aec7b430b49574628113b264558e0bf228e438f6
Tobsic <s0539713@htw-berlin.de>
2015-07-03 16:46:05 +0200

    pagination class annotation added

--- app/util/Pagination.java
+++ app/util/Pagination.java
@@ -1,7 +1,16 @@
 package util;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by Tobsic on 03.07.2015.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Pagination {
+    int pageSize() default 100;
+    int maxPageSize() default 500;
 }
