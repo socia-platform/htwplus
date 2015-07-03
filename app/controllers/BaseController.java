00a906cada49b92182b88273d4d3f55e098ee8f2
Tobsic <s0539713@htw-berlin.de>
2015-07-03 18:23:16 +0200

    added methods to get uri (base/full)

--- app/controllers/BaseController.java
+++ app/controllers/BaseController.java
@@ -3,6 +3,8 @@ package controllers;
 import play.db.jpa.Transactional;
 import play.mvc.*;
 
+import java.net.URI;
+
 @Transactional
 @With(Component.class)
 public class BaseController extends Controller {
@@ -12,4 +14,19 @@ public class BaseController extends Controller {
         return status(status);
     }
 
+    public static URI getBaseUri() {
+        Http.Request request = request();
+        // I don't like the way they did it, but that's the way this framework fixed the protocol thing
+        // https://github.com/playframework/playframework/issues/842
+        String protocol = request.secure() ? "https://" : "http://";
+        return URI.create(protocol + request.host() + request.path());
+    }
+
+    public static URI getFullUri() {
+        Http.Request request = request();
+        // I don't like the way they did it, but that's the way this framework fixed the protocol thing
+        // https://github.com/playframework/playframework/issues/842
+        String protocol = request.secure() ? "https://" : "http://";
+        return URI.create(protocol + request.host() + request.uri());
+    }
 }
