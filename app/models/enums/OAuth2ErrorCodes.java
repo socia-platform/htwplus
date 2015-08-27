b416abd48430eeb71e7677f0a27368fbbd8d2814
Roechi <msg.remus@gmail.com>
2015-08-27 21:21:56 +0200

    Added OAuth2 error codes.

---
+++ app/models/enums/OAuth2ErrorCodes.java
@@ -0,0 +1,30 @@
+package models.enums;
+
+/**
+ * Created by richard on 27.08.15.
+ */
+public enum OAuth2ErrorCodes {
+    INVALID_REQUEST("invalid_request"),
+    INVALID_CLIENT("invalid_client"),
+    UNAUTHORIZED_CLIENT("unauthorized_client"),
+    REDIRECT_URI_MISMATCH("redirect_uri_mismatch"),
+    ACCESS_DENIED("access_denied"),
+    UNSUPPORTED_RESPONSE_TYPE("unsupported_response_type"),
+    INVALID_SCOPE("invalid_scope")
+    ;
+
+
+    private String identifier;
+
+    OAuth2ErrorCodes(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public String getAcceptHeaderMessage() {
+        return "Only accepting Accept header: " + CustomContentType.JSON_COLLECTION.getIdentifier();
+    }
+}
