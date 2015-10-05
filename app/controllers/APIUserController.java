d1e60d04d26197175358df2a2499570df3730c7b
Richard Remus <richard.remus@idealo.de>
2015-10-05 21:23:34 +0200

    Added filtering for friendship.

--- app/controllers/APIUserController.java
+++ app/controllers/APIUserController.java
@@ -1,7 +1,11 @@
 package controllers;
 
 import models.Account;
+import models.Friendship;
 import models.enums.CustomContentType;
+import net.hamnaberg.funclite.Predicate;
+import net.hamnaberg.json.Collection;
+import net.hamnaberg.json.Item;
 import play.db.jpa.Transactional;
 import play.mvc.Result;
 import play.mvc.Security;
@@ -13,7 +17,27 @@ public class APIUserController extends BaseController {
     public static Result get(final long id) {
         if (request().getHeader("Accept").contains(CustomContentType.JSON_COLLECTION.getIdentifier())) {
             response().setContentType(CustomContentType.JSON_COLLECTION.getIdentifier());
-            return ok(JsonCollectionUtil.getRequestedCollectionString(Account.class, id, "Account"));
+            Collection collection = JsonCollectionUtil.getRequestedCollection(Account.class, id, "Account");
+            Collection filterdCollection = new Collection.Builder().addItems(collection.filterItems(new Predicate<Item>() {
+                @Override
+                public boolean apply(Item item) {
+                    long tokenUser = Long.parseLong(session().get("token_user"));
+                    Account tokenUserAccount = Account.findById(tokenUser);
+                    boolean isFriend = false;
+                    for (Friendship f : tokenUserAccount.friends) {
+                        if (f.friend.id == item.getData().getDataAsMap().get("id").getValue().get().asNumber().longValue()) {
+                            isFriend = true;
+                        }
+                    }
+                    return isFriend;
+                }
+            })).withHref(collection.getHref().get())
+                    .withError(collection.getError().get())
+                    .withTemplate(collection.getTemplate().get())
+                    .addLinks(collection.getLinks())
+                    .addQueries(collection.getQueries())
+                    .build();
+            return ok(filterdCollection.toString());
         } else {
             return statusWithWarning(NOT_ACCEPTABLE, CustomContentType.JSON_COLLECTION.getAcceptHeaderMessage());
         }
