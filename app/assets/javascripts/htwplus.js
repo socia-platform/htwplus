54fe22d4ffd2f60b1041d79219b1a1db4c6140d6
IvenZ <iven@posteo.de>
2016-03-17 17:04:58 +0100

    #140 install markdownit.js and some plugins; enable markdown for posts and comments

--- app/assets/javascripts/htwplus.js
+++ app/assets/javascripts/htwplus.js
@@ -126,6 +126,17 @@ function showErrorBeforeElement(element, error_message) {
     element.before('<div class="alert alert-danger"><a data-dismiss="alert" class="close" href="#">×</a>'+error_message+'</div>');
 }
 
+/** markdown post content */
+function markdownPostContent() {
+    $('.hp-truncate').each(function( index, value ) {
+        // check if element is already marked (pagination issue)
+        if (!$(this).hasClass('marked')) {
+            this.innerHTML = md.render(value.textContent);
+            $(this).addClass('marked');
+        }
+    });
+}
+
 /*
  *  Options Menu
  */
@@ -174,9 +185,9 @@ $('body').on('click', 'a.hp-post-edit', function(e) {
         var post_container = $("#"+post_id);
 
         var old_content = replaceContentWithLoadingIndicator(post_container);
+        post_container.removeClass('marked');
         var removed_classes = post_container.attr("class");
         post_container.attr("class", ""); // remove the classes (preventing linkify and whitespace stuff to apply)
-
         post_container.load("/post/"+post_id+"/getEditForm", function(response, status, xhr) {
             if (status == "error") {
                 console.log("Error when trying to edit post: ["+status+"]");
@@ -280,7 +291,23 @@ $('body').on('click', '.hp-mediaList-submit', function (e) {
     $('#mediaListFrom').append('<input type="hidden" name="action" value="delete">').submit();
 });
 
+/*
+ * Markdown definition
+ */
+var md = window.markdownit({
+               html: false,
+               breaks: true,
+               linkify: false,
+               typographer: true
+             }).use(window.markdownitMark).use(window.markdownitEmoji);
+
+md.renderer.rules.emoji = function(token, idx) {
+  return '<img class="emoji" width="20" height="20" src="' + location.origin + '/assets/images/emojis/' + token[idx].markup + '.png" />';
+};
+
 $(document).ready(function () {
+
+    markdownPostContent();
 
     /*
      *  Token
@@ -309,6 +336,7 @@ $(document).ready(function () {
 		},
 		complete:function(){
 			$(".loading").hide();
+			markdownPostContent();
 			autolinkUrls();
 			linkOlderComments();
 			linkAddComments();
@@ -396,12 +424,12 @@ $(document).ready(function () {
                     var custom_avatar = false;
                     if(item._type === 'user') {
                         label = item._source.name;
-                        hLabel = item.highlight.name;
+                        hLabel = item.highlight.name[0].replace('[startStrong]', '<strong>').replace('[endStrong]', '</strong>');
                         if(item._source.avatar === 'custom') {custom_avatar = true;}
                     }
                     if(item._type === 'group') {
                         label = item._source.title;
-                        hLabel = item.highlight.title;
+                        hLabel = item.highlight.title[0].replace('[startStrong]', '<strong>').replace('[endStrong]', '</strong>');
                         groupType = item._source.grouptype;
                         if(item._source.avatar) {
                             custom_avatar = true;
--- app/models/services/ElasticsearchService.java
+++ app/models/services/ElasticsearchService.java
@@ -262,8 +262,7 @@ public class ElasticsearchService implements IElasticsearchService {
         }
 
         // Define html tags for highlighting
-        if (caller.equals("search"))
-            searchRequest = searchRequest.setHighlighterPreTags("[startStrong]").setHighlighterPostTags("[endStrong]").setHighlighterNumOfFragments(0);
+        searchRequest = searchRequest.setHighlighterPreTags("[startStrong]").setHighlighterPostTags("[endStrong]").setHighlighterNumOfFragments(0);
 
         // Enable pagination
         searchRequest = searchRequest.setFrom((page * ES_RESULT_SIZE) - ES_RESULT_SIZE);
